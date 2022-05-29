3a6c117bd523ce851f7e29ee1c02fae6d9e43051
natmrcl <91374949+natmrcl@users.noreply.github.com>
2022-05-29 23:54:24 +0700

    Update A_5025201238.r

--- A_5025201238.r
+++ A_5025201238.r
@@ -48,7 +48,37 @@ qt(p = 0.025, df = 2, lower.tail = FALSE)
 #f. Kesimpulan
 #Berdasarkan pengujian di atas, rata-rata saham Bandung dan Bali adalah sama.
 
-#NOMOR 4 
+#NOMOR 4
+dataset = read.table("onewayanova.txt",h=T)
+attach(dataset)
+
+#a. Buatlah masing masing jenis spesies menjadi 3 subjek "Grup" (grup 1,grup 2,grup 3). Lalu Gambarkan plot kuantil normal untuk setiap kelompok dan lihat apakah ada outlier utama dalam homogenitas varians.
+dataset$Group = as.factor(dataset$Group)
+dataset$Group = factor(dataset$Group,labels = c("Kucing Oren", "Kucing Hitam", "Kucing Putih"))
+class(dataset$Group)
+
+Grup1 = subset(dataset, Group == "Kucing Oren")
+Grup2 = subset(dataset, Group == "Kucing Hitam")
+Grup3 = subset(dataset, Group == "Kucing Putih")
+
+#b. carilah atau periksalah Homogeneity of variances nya , Berapa nilai p yang didapatkan? , Apa hipotesis dan kesimpulan yang dapat diambil ?
+bartlett.test(Length ~ Group, data = dataset)
+
+#c. Untuk uji ANOVA (satu arah), buatlah model linier dengan Panjang versus Grup dan beri nama model tersebut model 1.
+model1 = lm(Length ~ Group, data = dataset)
+anova(model1)
+
+#d. Dari Hasil Poin C, Berapakah nilai-p ? , Apa yang dapat Anda simpulkan dari H0?
+
+#e. Verifikasilah jawaban model 1 dengan Post-hoc test Tukey HSD, dari nilai p yang didapatkan apakah satu jenis kucing lebih panjang dari yang lain? Jelaskan.
+anv = aov(model1)
+tukey = TukeyHSD(anv)
+tukey
+
+#f. Visualisasikan data dengan ggplot2
+ggplot(dataset, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey", colour = "black") +
+scale_x_discrete() + ylab("Length (cm)")
+
 
 #NOMOR 5
 library(readr)
